4c07dca00137b7ecb44350d27d3b9640c9bfe030
Rodolpho T. Sotolani <rt.sotolani@gmail.com>
2024-10-16 21:22:51 -0400

    feature: Create a new project

---
+++ src/main/java/br/com/rts/estudos/graphql/client/ClientExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.rts.estudos.graphql.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientExampleApplication.class, args);
+    }
+
+}
